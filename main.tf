9d13e0015fc647f55707c30f8e280d3382317b12
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2022-04-18 17:01:49 +0200

    added tags

--- main.tf
+++ main.tf
@@ -5,4 +5,5 @@ provider "azurerm" {
 resource "azurerm_resource_group" "codetalks-rg" {
   name     = "codetalks-rg"
   location = var.AZ_Location
+  tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "AZ_Location" {
   type    = string
   default = "westeurope"
 }
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Source = "Terraform"
+    Environment = "CodeTalks"
+    Author = "Milos Slavic"
+  }
+}

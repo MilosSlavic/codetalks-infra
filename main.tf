13ae44aa847f10b3cd41acd73fb15ac4561889f5
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2022-04-19 11:33:40 +0200

    fixes

--- main.tf
+++ main.tf
@@ -24,17 +24,15 @@ resource "azurerm_key_vault" "codetalks-keyvault" {
   soft_delete_retention_days      = 7
   purge_protection_enabled        = false
   sku_name                        = "standard"
-  access_policy                   = []
-  tags                            = var.tags
-}
-
-resource "azurerm_key_vault_access_policy" "codetalks-keyvault-accesspolicy" {
-  key_vault_id        = azurerm_key_vault.codetalks-keyvault.id
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  object_id           = data.azurerm_client_config.current.object_id
-  key_permissions     = ["Get"]
-  secret_permissions  = ["Get"]
-  storage_permissions = ["Get"]
+  access_policy {
+    tenant_id               = data.azurerm_client_config.current.tenant_id
+    object_id               = data.azurerm_client_config.current.object_id
+    key_permissions         = ["Get"]
+    secret_permissions      = ["Get"]
+    storage_permissions     = ["Get"]
+    certificate_permissions = ["Get"]
+  }
+  tags = var.tags
 }
 
 resource "azurerm_virtual_network" "codetalks-vnet" {
@@ -46,9 +44,9 @@ resource "azurerm_virtual_network" "codetalks-vnet" {
 }
 
 resource "azurerm_subnet" "k8s-subnet" {
-  name                 = "k8s-subnet"
-  resource_group_name  = azurerm_resource_group.codetalks-rg.name
-  virtual_network_name = azurerm_virtual_network.codetalks-vnet.name
-  address_prefixes     = ["10.0.1.0/16"]
-  service_endpoints = [ "Microsoft.ContainerRegistry", "Microsoft.KeyVault", "Microsoft.Storage" ]
+  name                        = "k8s-subnet"
+  resource_group_name         = azurerm_resource_group.codetalks-rg.name
+  virtual_network_name        = azurerm_virtual_network.codetalks-vnet.name
+  address_prefixes            = ["10.0.0.0/16"]
+  service_endpoints           = ["Microsoft.ContainerRegistry", "Microsoft.KeyVault", "Microsoft.Storage"]
 }

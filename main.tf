62e65e0d4b4e5c79a02061ef452549a4523935b4
MilosSlavic <62957449+MilosSlavic@users.noreply.github.com>
2022-05-10 10:09:57 +0200

    Rename and remove vnet

--- main.tf
+++ main.tf
@@ -8,16 +8,16 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "codetalks-rg" {
+resource "azurerm_resource_group" "rg" {
   name     = "codetalks-rg"
   location = var.AZ_Location
   tags     = var.tags
 }
 
-resource "azurerm_key_vault" "codetalks-keyvault" {
+resource "azurerm_key_vault" "keyvault" {
   name                            = "codetalk-keyvault"
-  location                        = azurerm_resource_group.codetalks-rg.location
-  resource_group_name             = azurerm_resource_group.codetalks-rg.name
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
   enabled_for_disk_encryption     = false
   enabled_for_template_deployment = true
   tenant_id                       = data.azurerm_client_config.current.tenant_id
@@ -43,45 +43,45 @@ resource "azurerm_key_vault" "codetalks-keyvault" {
   tags = var.tags
 }
 
-resource "azurerm_virtual_network" "codetalks-vnet" {
-  name                = "codetalks-vnet"
-  location            = azurerm_resource_group.codetalks-rg.location
-  resource_group_name = azurerm_resource_group.codetalks-rg.name
-  address_space       = ["10.0.0.0/8"]
-  tags                = var.tags
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "codetalks-vnet"
+#   location            = azurerm_resource_group.codetalks-rg.location
+#   resource_group_name = azurerm_resource_group.codetalks-rg.name
+#   address_space       = ["10.0.0.0/8"]
+#   tags                = var.tags
+# }
 
-resource "azurerm_subnet" "k8s-subnet" {
-  name                 = "k8s-subnet"
-  resource_group_name  = azurerm_resource_group.codetalks-rg.name
-  virtual_network_name = azurerm_virtual_network.codetalks-vnet.name
-  address_prefixes     = ["10.0.0.0/16"]
-  service_endpoints    = ["Microsoft.ContainerRegistry", "Microsoft.KeyVault", "Microsoft.Storage"]
-}
+# resource "azurerm_subnet" "subnet" {
+#   name                 = "k8s-subnet"
+#   resource_group_name  = azurerm_resource_group.codetalks-rg.name
+#   virtual_network_name = azurerm_virtual_network.codetalks-vnet.name
+#   address_prefixes     = ["10.0.0.0/16"]
+#   service_endpoints    = ["Microsoft.ContainerRegistry", "Microsoft.KeyVault", "Microsoft.Storage"]
+# }
 
 resource "azurerm_container_registry" "acr" {
   name                          = "codetalksacr"
-  resource_group_name           = azurerm_resource_group.codetalks-rg.name
-  location                      = azurerm_resource_group.codetalks-rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
   public_network_access_enabled = false
   sku                           = "Premium"
   zone_redundancy_enabled       = false
 
-  network_rule_set {
-    default_action = "Deny"
-    virtual_network {
-      action    = "Allow"
-      subnet_id = azurerm_subnet.k8s-subnet.id
-    }
-  }
+  # network_rule_set {
+  #   default_action = "Deny"
+  #   virtual_network {
+  #     action    = "Allow"
+  #     subnet_id = azurerm_subnet.k8s-subnet.id
+  #   }
+  # }
 
   tags = var.tags
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "codetalks-aks"
-  location            = azurerm_resource_group.codetalks-rg.location
-  resource_group_name = azurerm_resource_group.codetalks-rg.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "codetalksaks"
   kubernetes_version  = "1.22.6"
 
